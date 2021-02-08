beda259fc9ded7ef21524f3983bf341a25a5fb1d
RyGuy <oboylery@gmail.com>
2021-02-07 22:54:47 -0500

    ocd

--- src/main/java/com/brawl/oldguns/events/GunExplodeEvent.java
+++ src/main/java/com/brawl/oldguns/events/GunExplodeEvent.java
@@ -1,16 +1,15 @@
 package com.brawl.oldguns.events;
 
 import com.brawl.oldguns.gun.Explosion;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class GunExplodeEvent extends GunEvent {
-    private final Explosion explosion;
 
-    public GunExplodeEvent(Explosion explosion) {
-        this.explosion = explosion;
-    }
+    private final Explosion explosion;
 
 }
